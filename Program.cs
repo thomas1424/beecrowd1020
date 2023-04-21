46d784a3f524ab0706545910ce82477db323c115
Thomas Paulo Jr <thomaspaulo@hotmail.com>
2023-04-21 17:59:12 -0300

    salvando uri1020

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace uri1020 { 
+class Program
+{
+
+        static void Main(string[] args)
+        {
+            int n, resto, anos, meses, dias;
+
+            n = int.Parse(Console.ReadLine());
+
+            anos = n / 365;
+            resto = n % 365;
+            meses = resto / 30;
+            dias = resto % 30;
+
+            Console.WriteLine(anos + " anos(s)");
+            Console.WriteLine(meses + " mes(es)");
+            Console.WriteLine(dias + " dia(s)");
+        }
+
+    }
+
+}
